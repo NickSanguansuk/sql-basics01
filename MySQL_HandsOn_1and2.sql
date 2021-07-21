ab6982ab6085bd61fbae251a656b7020820cc8df
Wasin Sanguansuk <wasin_sanguansuk@hotmail.com>
2021-07-21 15:57:12 -0500

    Testing second commit

--- MySQL_HandsOn_1and2.sql
+++ MySQL_HandsOn_1and2.sql
@@ -189,7 +189,7 @@ ORDER BY SUM(od.quantityOrdered * od.priceEach) DESC;
 --		 The first column should be labeled ‘Month’, the second ‘Year’, and the third should be ‘Payments Received’. 
 --	  	 Values in the third column should be formatted as numbers with two decimals – for example: 694,292.68.
 
-
+SELECT 
 
 
 
